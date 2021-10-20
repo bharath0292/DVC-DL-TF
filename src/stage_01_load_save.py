bd1a82f6966f8664c5b052c19a5cca66cb2c5929
Bharath <bharath0292@gmail.com>
2021-10-20 16:25:22 +0530

    log function and file added

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,8 +1,16 @@
+from genericpath import exists
 from src.utils.all_utils import read_yaml,create_directory
 import argparse
 import os
 import shutil
 from tqdm import tqdm
+import logging
+
+logging_str="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir="logs"
+os.makedirs(log_dir,exist_ok=True)
+filename=os.path.join(log_dir,"running_logs.log")
+logging.basicConfig(filename=filename, level=logging.INFO, format=logging_str, filemode='a')
 
 def copy_file(source_download_dir,local_data_dir):
     list_of_files=os.listdir(source_download_dir)
@@ -32,4 +40,13 @@ if __name__=='__main__':
     
     parsed_args=args.parse_args()
 
-    get_data(parsed_args.config)
+    try:
+        logging.info(">>>> stage 01 started")
+
+        get_data(parsed_args.config)
+
+        logging.info("stage 01 completed and all the data are saved in local >>>>")
+
+    except Exception as e:
+        logging.exception(e)
+        raise e
