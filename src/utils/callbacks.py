6684e56a9f98d37264a123feeee944329cfdaf66
Bharath <bharath0292@gmail.com>
2021-11-09 15:03:34 +0530

    tensorboard callback added

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -1,7 +1,23 @@
+import os
+import tensorflow as tf
+import time
+import joblib
+import logging
+from src.utils import get_timestamp
+
 
 
 def create_and_save_tensorboard_callback(callbacks_dir,tensorboard_log_dir):
-    pass
+    unique_name = get_timestamp("tb_logs")
+
+    tb_running_log_dir = os.path.join(tensorboard_log_dir,unique_name)
+    tensorboard_callback = tf.keras.callbacks.Tensorboard(log_dir=tb_running_log_dir)
+
+    tb_callback_filepath = os.path.joins(callbacks_dir, "tensorboard_cb.cb")
+    
+    joblib.dump(tensorboard_callback,tb_callback_filepath)
+
+    logging.info(f"Tensorboard callback is being saved at {tb_callback_filepath}")
 
 def create_and_save_checkpoint_callback(checkpoint_dir,tensorboard_log_dir):
     pass
