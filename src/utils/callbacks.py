89e67013cd4c0e7ef08d9dc764aa6384b434e365
Bharath <bharath0292@gmail.com>
2021-11-09 18:09:14 +0530

    stage 03 mistakes corrected

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -3,17 +3,16 @@ import tensorflow as tf
 import time
 import joblib
 import logging
-from src.utils import get_timestamp
-
+from src.utils.all_utils import get_timestamp
 
 
 def create_and_save_tensorboard_callback(callbacks_dir,tensorboard_log_dir):
     unique_name = get_timestamp("tb_logs")
 
     tb_running_log_dir = os.path.join(tensorboard_log_dir,unique_name)
-    tensorboard_callback = tf.keras.callbacks.Tensorboard(log_dir=tb_running_log_dir)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
 
-    tb_callback_filepath = os.path.joins(callbacks_dir, "tensorboard_cb.cb")
+    tb_callback_filepath = os.path.join(callbacks_dir, "tensorboard_cb.cb")
     
     joblib.dump(tensorboard_callback,tb_callback_filepath)
 
@@ -28,7 +27,7 @@ def create_and_save_checkpoint_callback(checkpoint_dir,tensorboard_log_dir):
         save_best_only = True
     )
 
-    ckpt_callback_filepath = os.path.joins(checkpoint_dir, "checkpoint_cb.cb")
+    ckpt_callback_filepath = os.path.join(checkpoint_dir, "checkpoint_cb.cb")
     
     joblib.dump(checkpoint_callback,ckpt_callback_filepath)
 
